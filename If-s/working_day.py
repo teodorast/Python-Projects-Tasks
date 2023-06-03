f53b8c5edc478b5d014c556ddd9221f75bb13365
teodorast <teodora.stefanova898@gmail.com>
2023-06-03 11:07:34 +0300

    for loops changes

---
+++ If-s/working_day.py
@@ -0,0 +1,17 @@
+working_day = input()
+if working_day == "Monday":
+    print("Working day")
+elif working_day == "Tuesday":
+    print("Working day")
+elif working_day == "Wednesday":
+    print("Working day")
+elif working_day == "Thursday":
+    print("Working day")
+elif working_day == "Friday":
+    print("Working day")
+elif working_day == "Saturday":
+    print("Weekend")
+elif working_day == "Sunday":
+    print("Weekend")
+else:
+    print("Error")
---
+++ for/even_powers_of_n.py
@@ -0,0 +1,3 @@
+number = int(input())
+for number in range(0,number+1,2):
+    print(2**number)
