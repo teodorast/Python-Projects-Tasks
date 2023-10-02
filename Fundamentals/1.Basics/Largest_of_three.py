8876b4cde51ff904a7a225b8012cefcd008fb841
teodorast <teodora.stefanova898@gmail.com>
2023-10-02 17:13:41 +0300

    basics revision

---
+++ Fundamentals/1.Basics/Largest_of_three.py
@@ -0,0 +1,10 @@
+firts_number = int(input())
+second_number = int(input())
+third_number=int(input())
+
+if firts_number>second_number and firts_number>third_number:
+    print(firts_number)
+elif second_number>firts_number and second_number >third_number:
+    print(second_number)
+else:
+    print(third_number)
---
+++ Fundamentals/1.Basics/Shopping.py
@@ -0,0 +1,20 @@
+budget = int(input())
+
+command = input()
+
+while command !="End":
+    prices = int(command)
+    budget-=prices
+
+
+    if budget<0:
+        print("You went in overdraft!")
+        break
+
+    
+    command = input()
+    
+
+else:
+    print("You bought everything needed.")
+
